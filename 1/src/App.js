a3f4a89face195cde7aaa112987c85503b907379
FilippK <filippkotenko@outlook.com>
2020-03-08 12:24:19 +0200

    new

---
+++ 1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Wrapper from "./wrapper";
+import './App.css';
+
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Wrapper/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 1/src/wrapper.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from "./wrapper.module.css";
+
+
+const Wrapper = () => {
+    return (
+        <div className={s.wrapper}>
+        <h3>Kotenko Filipp Sergeevych</h3>
+        </div>
+    );
+}
+
+export default Wrapper;
