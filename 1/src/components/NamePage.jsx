fc6bc6513c88d6c5f9afb39238fb36e51c22e0c7
FIlipp <filippkotenko@outlook.com>
2020-04-04 23:45:15 +0300

    Fonts

--- 1/src/components/NamePage.jsx
+++ 1/src/components/NamePage.jsx
@@ -4,7 +4,7 @@ import s from './NamePage.module.css'
 const NamePage = () => {
      return (
             <div className={s.nameFil}>
-                <p>Котенко Филипп</p>
+                <p>Котенко Филипп Сергеевич</p>
             </div>
         );
     }
